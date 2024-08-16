95dfb548558b7dc84f5aff68c4a68cf7bfade239
CAMNWK\nin100 <nin100@cambiumnetworks.com>
2024-08-16 07:27:51 +0530

    Separator in print command

---
+++ src/13Aug/Homework.py
@@ -0,0 +1,29 @@
+# string1 = "I like %s" % "Python"
+# print(string1)
+#
+# temp = "Indra"
+# string2 = "The name is %s" % temp
+# print(string2)
+
+#####Task1 #####
+
+# num = int(input("Give the number:"))
+# for i in range(1,11):
+#     num2 = num * i
+#     print(num," * ",i,"=", num2)
+
+
+####Task2 ######
+
+num3 = int(input("Enter first number:"))
+num4 = int(input("Enter second number:"))
+
+print("Max is ", max(num3, num4))
+print("power is", pow(num3,num4))
+print(num3-num4)
+print(num3+num4)
+print(num3*num4)
+print(num3/num4)
+
+
+
